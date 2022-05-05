61eba9b2cf1bdb306cd876f728bbc161f344bd88
benitobronzi <benito.bronzi@gmail.com>
2022-05-05 14:33:19 +0200

    files upload 3

--- app/view/addphoto.php
+++ app/view/addphoto.php
@@ -1,6 +1,6 @@
 <div class="nosession">
     <div> <br><br><br><br><br><br><br><br><br><br>
-        <form action="index.php?c=addphoto&a=addphoto" method="post" enctype="multipart/form-data">
+        <form action="index.php?c=addphoto&a=add" method="post" enctype="multipart/form-data">
             <p>Images:
                 <input type="file"  id="image" name="image" accept="image/*">
                 <input type="submit" value="Send">
